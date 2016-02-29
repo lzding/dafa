4e8d780d8ba85cf98890658302f9ad3aebc8d4de
lzding <472339105@qq.com>
2016-02-29 14:40:00 +0800

    coding

---
+++ web/app/assets/javascripts/application.js
@@ -0,0 +1,21 @@
+// This is a manifest file that'll be compiled into application.js, which will include all the files
+// listed below.
+//
+// Any JavaScript/Coffee file within this directory, lib/assets/javascripts, vendor/assets/javascripts,
+// or any plugin's vendor/assets/javascripts directory can be referenced here using a relative path.
+//
+// It's not advisable to add code directly here, but if you do, it'll appear at the bottom of the
+// compiled file.
+//
+// Read Sprockets README (https://github.com/rails/sprockets#sprockets-directives) for details
+// about supported directives.
+//
+//= require turbolinks
+//= require jquery-1.11.2.min
+//= require jquery.ui/jquery-ui-1.9.1.custom.min
+//= require jquery.ui/jquery.datetimepicker
+//= require jquery.file.upload/jquery.iframe-transport
+//= require jquery.file.upload/jquery.fileupload
+//= require bootstrap.min.js
+//= require jquery_ujs
+//= require_tree .
